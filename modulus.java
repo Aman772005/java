ce1e1c902e082c0212041f81d9dfbb2a30cefc13
Aman772005 <amansinghtomar772005@gmail.com>
2026-05-19 21:05:03 +0530

    update

--- modulus.java
+++ modulus.java
@@ -1,4 +1,22 @@
-// *** modulus operator ***
+// // *** modulus operator ***
+
+
+// import java.util.Scanner;
+
+// public class modulus{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter a : ");
+//         int a = sc.nextInt();
+
+//         System.out.print("Enter b : ");
+//         int b = sc.nextInt();
+
+//         int ans = a % b ;
+//         System.out.print("a % b = "+ ans);
+//     }
+// }
 
 
 import java.util.Scanner;
@@ -17,3 +35,4 @@ public class modulus{
         System.out.print("a % b = "+ ans);
     }
 }
+
