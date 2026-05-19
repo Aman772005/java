17c0a15032ebea8a14cf0193c1b2a176760f07f4
Aman772005 <amansinghtomar772005@gmail.com>
2026-05-19 21:03:42 +0530

    modulus operator

---
+++ modulus.java
@@ -0,0 +1,19 @@
+// *** modulus operator ***
+
+
+import java.util.Scanner;
+
+public class modulus{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a : ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter b : ");
+        int b = sc.nextInt();
+
+        int ans = a % b ;
+        System.out.print("a % b = "+ ans);
+    }
+}
