e6feed30d8f90ec8aca18bb432de89dbb9aa2395
Aman772005 <amansinghtomar772005@gmail.com>
2026-05-25 12:25:14 +0530

    loops

---
+++ loops.java
@@ -0,0 +1,100 @@
+// public class loops {
+//     public static void main(String[] args){
+//         for(int i = 1 ; i<=3 ; i++){
+//             System.out.println("Aman");
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+// public class loops{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int i ;
+//         for(i=1;i<=100;i++){
+//             if(i%2 == 0){
+//                 System.out.println(i);
+//             }
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+// public class loops{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int i ;
+//         for(i=17;i<=170;i=i+17){
+//                 System.out.println(i);
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+// public class loops{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int i ;
+//         for(i=3;i<=100;i=i+6){
+
+//             // if(i % 2 != 0 && i%3 == 0){
+//             //     System.out.println(i);
+//             // }
+//                  System.out.println(i);
+
+//         }
+//     }
+// }
+
+
+// import java.util.Scanner;
+// public class loops{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int i ;
+//         int n = sc.nextInt();
+//         for(i=n;i<=1;i++){
+
+//             // if(i % 2 != 0 && i%3 == 0){
+//             //     System.out.println(i);
+//             // }
+//              System.out.println(i);
+
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+// public class loops{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int i ;
+//         int n = sc.nextInt();
+//         for(i=2; i<= 3*n;i=i+3){
+
+//             // if(i % 2 != 0 && i%3 == 0){
+//             //     System.out.println(i);
+//             // }
+//              System.out.println(i);
+
+//         }
+//         System.out.println(i);
+//     }
+// }
+
+import java.util.Scanner;
+public class loops{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int i ;
+        // int n = sc.nextInt();
+        for(i=99; i>0;i=i-4){
+
+            // if(i % 2 != 0 && i%3 == 0){
+            //     System.out.println(i);
+            // }
+             System.out.println(i);
+
+        }
+    }
+}
