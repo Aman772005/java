58724670ac6b4369c4a3c44b3f51b1b29f0ee8fa
Aman772005 <amansinghtomar772005@gmail.com>
2026-05-26 13:15:50 +0530

    loops

--- loops.java
+++ loops.java
@@ -82,19 +82,73 @@
 //     }
 // }
 
+
+
+// import java.util.Scanner;
+// public class loops{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int i ;
+        
+//         for(i=99; i>0;i=i-4){
+
+            
+//              System.out.println(i);
+
+//         }
+//     }
+// }
+
+
+// import java.util.Scanner;
+// public class loops{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int i ;
+//         int a = 1 ; 
+//         int r = 2;
+//         int n = sc.nextInt();
+        
+//         for(i = 1; i<=n;i++){
+            
+//             System.out.println(a);
+//             a = a*r ;
+
+//         }
+//     }
+//
+
+
+// import java.util.Scanner;
+// public class loops{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int i=1;i<=n;i++){
+//             if(i%2 != 0){
+//                 System.out.println(i);
+//             }else if(i%2 == 0){
+//                 System.out.println(n);
+//                 n = n-1;
+//             }
+//         }
+//     }
+// }
+
 import java.util.Scanner;
 public class loops{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int i ;
-        // int n = sc.nextInt();
-        for(i=99; i>0;i=i-4){
+        
 
-            // if(i % 2 != 0 && i%3 == 0){
-            //     System.out.println(i);
-            // }
-             System.out.println(i);
-
+        for(int i=65;i<=90;i++){
+            System.out.println((char)i + " " + i);
         }
     }
 }
+
+
+
+
+
