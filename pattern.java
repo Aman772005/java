8d112d5e5308aa7266ed803d8c2f0eeda7fbf6d7
Aman772005 <amansinghtomar772005@gmail.com>
2026-06-09 17:21:04 +0530

    cross pattern

--- pattern.java
+++ pattern.java
@@ -160,27 +160,52 @@
 //         }
 
 //     }
+// // }
+
+// import java.util.Scanner;
+
+// public class pattern{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         int a = sc.nextInt();
+//         int n = a/2 ;
+//         n = n+1 ;
+
+//         System.out.println(n);
+
+//         for(int i = 1 ; i<=a;i++){
+//             for(int j = 1;j<=a;j++){
+//                 if(i==n || j==n){
+//                     System.out.print("* ");
+//                 }else{
+//                     System.out.print("  ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
 // }
 
+// }
+
+
 import java.util.Scanner;
-
-public class pattern{
-    public static void main(String[] args){
+public class pattern {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
 
-        int a = sc.nextInt();
-        int n = a/2 ;
-        n = n+1 ;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
 
-        System.out.println(n);
-
-        for(int i = 1 ; i<=a;i++){
-            for(int j = 1;j<=a;j++){
-                if(i==n || j==n){
+                if (i == j || i + j == n - 1) {
                     System.out.print("* ");
-                }else{
+                } else {
                     System.out.print("  ");
                 }
+
             }
             System.out.println();
         }
