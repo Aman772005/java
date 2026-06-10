06046234b31919bed6d821028d0ce4dd3e4da223
Aman772005 <amansinghtomar772005@gmail.com>
2026-06-10 18:17:46 +0530

    Binary triangle

--- pattern.java
+++ pattern.java
@@ -190,6 +190,46 @@
 // }
 
 
+// import java.util.Scanner;
+// public class pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+        
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+
+//                 if (i == j || i + j == n - 1) {
+//                     System.out.print("* ");
+//                 } else {
+//                     System.out.print("  ");
+//                 }
+
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+// public class pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int a = 1;
+        
+
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print(a);
+//                 a++;
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
 import java.util.Scanner;
 public class pattern {
     public static void main(String[] args) {
@@ -197,15 +237,13 @@ public class pattern {
         int n = sc.nextInt();
         
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-
-                if (i == j || i + j == n - 1) {
-                    System.out.print("* ");
-                } else {
-                    System.out.print("  ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if((i+j)%2 == 0){
+                    System.out.print("1");
+                }else{
+                    System.out.print("0");
                 }
-
             }
             System.out.println();
         }
