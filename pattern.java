fccd2c70c61b1fa9b97dfb86efc01069671b3cc9
Aman772005 <amansinghtomar772005@gmail.com>
2026-06-10 21:24:22 +0530

    vertical Flipped triangle

--- pattern.java
+++ pattern.java
@@ -230,22 +230,109 @@
 //     }
 // }
 
+// import java.util.Scanner;
+// public class pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+        
+
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= i; j++) {
+//                 if((i+j)%2 == 0){
+//                     System.out.print("1");
+//                 }else{
+//                     System.out.print("0");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// import java.util.Scanner;
+// public class pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int a = 1;
+        
+
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print(a);
+//                 a=a+2;
+//             }
+//             a=1;
+//             System.out.println();
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+// public class pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+       
+        
+
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= n; j++) {
+//                 if(i+j<=4){
+//                     System.out.print(" ");
+//                 }else System.out.print("*");
+//             }
+             
+//             System.out.println();
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+// public class pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+       
+        
+
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= n-i; j++){
+//                 System.out.print(" ");
+//             }
+//             for (int j = 1; j <= i; j++){
+//                 System.out.print("*");
+//             }
+             
+//             System.out.println();
+//         }
+//     }
+// }
+
+
 import java.util.Scanner;
 public class pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
+        int a = 1;
+       
         
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                if((i+j)%2 == 0){
-                    System.out.print("1");
-                }else{
-                    System.out.print("0");
-                }
+            for (int j = 1; j <= n-i; j++){
+                System.out.print(" ");
             }
+            for (int j = 1; j <= i; j++){
+                System.out.print((char)(i+64));
+                a++;
+            }
+             a= 1;
             System.out.println();
         }
     }
 }
+
