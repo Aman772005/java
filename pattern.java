33f92acc3c37e971916845787684a45bb433ea5c
Aman772005 <amansinghtomar772005@gmail.com>
2026-06-11 22:49:58 +0530

    rombush , pyramid

--- pattern.java
+++ pattern.java
@@ -312,6 +312,74 @@
 // }
 
 
+// import java.util.Scanner;
+// public class pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+//         int a = 1;
+       
+        
+
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= n-i; j++){
+//                 System.out.print(" ");
+//             }
+//             for (int j = 1; j <= i; j++){
+//                 System.out.print((char)(i+64));
+//                 a++;
+//             }
+//              a= 1;
+//             System.out.println();
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+// public class pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+//         int a = 1;
+       
+        
+
+//         for(int i = 1 ; i<=n;i++){
+//             for(int j = 1;j<= n-i;j++){
+//                 System.out.print(" "+" ");
+//             }
+//             for(int j = 1; j<=n;j++){
+//                 System.out.print("*"+" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+// public class pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+//         int a = 1;
+       
+        
+
+//         for(int i = 1 ; i<=n;i++){
+//             for(int j = 1;j<= i-1;j++){
+//                 System.out.print(" "+" ");
+//             }
+//             for(int j = 1; j<=n-i+1;j++){
+//                 System.out.print("*"+" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
 import java.util.Scanner;
 public class pattern {
     public static void main(String[] args) {
@@ -322,17 +390,17 @@ public class pattern {
        
         
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n-i; j++){
-                System.out.print(" ");
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=n-i;j++){
+                System.out.print(" "+" ");
             }
-            for (int j = 1; j <= i; j++){
-                System.out.print((char)(i+64));
-                a++;
+            for(int j= 1;j<=(2*i-1);j++){
+                System.out.print("*"+" ");
             }
-             a= 1;
-            System.out.println();
+            System.out.println(" ");
         }
     }
 }
+
+
 
