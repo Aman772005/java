76fc626c8937d3c4ecde3c3f66caebc3e1717e6b
Aman772005 <amansinghtomar772005@gmail.com>
2026-06-13 17:16:47 +0530

    Arguments , syntax of functions

---
+++ lecture_5/Arguments.java
@@ -0,0 +1,10 @@
+package lecture_5;
+
+public class Arguments {
+    public static void sum(int a , int b) { // parameters
+        System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+        sum(3,5); // arguments 
+    }
+}
---
+++ lecture_5/syntax.java
@@ -0,0 +1,17 @@
+package lecture_5;
+
+public class syntax {
+
+    public static void aman(){
+        arun();
+        System.out.println("priya");
+    }
+    public static void main(String[] args) {
+        aman();
+        System.out.println("neha");
+    }
+
+    public static void arun(){
+        System.out.println("sneha");
+    }
+}
