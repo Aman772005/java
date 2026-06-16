333e45fc69f6ac9efb1a251ab5e7f14931ebaeab
Aman772005 <amansinghtomar772005@gmail.com>
2026-06-16 16:23:29 +0530

    Arrary

--- lecture_5/returntype.java
+++ lecture_5/returntype.java
@@ -44,23 +44,23 @@ package lecture_5;
 //         System.out.println(ncr+" "+npr);
 //     }
 // }
-import java.util.Scanner;
-public class returntype {
+// import java.util.Scanner;
+// public class returntype {
 
-    public static void swap(int a , int b) {
-        int temp = a;
-        a = b;
-        b = temp;
+//     public static void swap(int a , int b) {
+//         int temp = a;
+//         a = b;
+//         b = temp;
 
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-         int a = sc.nextInt();
-         int b = sc.nextInt();
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//          int a = sc.nextInt();
+//          int b = sc.nextInt();
 
 
-        System.out.println(a+ " "+b);
-        swap(a,b);
-        System.out.println(a+ " "+b);
-    }
-}
+//         System.out.println(a+ " "+b);
+//         swap(a,b);
+//         System.out.println(a+ " "+b);
+//     }
+// }
---
+++ lecture_6/Array.java
@@ -0,0 +1,51 @@
+package lecture_6;
+
+import java.util.Scanner;
+public class Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int[] x = {6,7,8,9,10};
+
+        // // indexing
+        // System.out.println(x[3]);
+        // // updating
+        // x[3]=89;
+        // System.out.println(x[3]);
+
+        // int[]  arr = new int[4];
+        // arr[0] = 2 ;
+        // arr[1] = 9 ;
+        // arr[2] = 4 ;
+        // arr[3] = 210 ;
+        // System.out.println(arr[0]);
+
+        // int arr[] = {5,6,9,23,121};
+        // int n = arr.length;
+        // System.out.println(n);
+        // for(int i=0;i<n;i++){
+        //     System.out.println(arr[i]);
+        // }
+
+        System.out.print("Enter array size : ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        int sumofarr = 0 ;
+
+        for(int i = 0;i<n;i++){
+            arr[i]= sc.nextInt();
+             sumofarr += arr[i] ;
+        }
+
+        System.out.println(sumofarr);
+
+
+        // for(int i = 0;i<n;i++){
+        //     if(arr[i]<0){
+        //         System.out.println(arr[i]);
+        //     }
+        // }
+
+    }
+}
