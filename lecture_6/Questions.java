d055905df0aec7cea073e9e399bb33733253c3c4
Aman772005 <amansinghtomar772005@gmail.com>
2026-06-26 17:03:50 +0530

    Two Sum

--- lecture_6/Questions.java
+++ lecture_6/Questions.java
@@ -24,20 +24,45 @@ package lecture_6;
 
 // *** question-2 ***
 
+// public class Questions {
+
+//     public static void main(String[] args) {
+//         int[] arr = {1,2,3,4,5,6,7,8,9,10};
+//         int x = 19 ;
+//         search(arr,x);
+//     }
+//     public static void search(int[] arr,int x) {
+//         for(int i=0;i<arr.length;i++){
+//             if(x==arr[i]) {
+//                 System.out.println("founded");
+//                  return ;
+//             }
+//         } 
+//         System.out.println("not founded");
+//     }
+// }
+
+
+// *** question-3 *** 
+
 public class Questions {
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9,10};
-        int x = 19 ;
-        search(arr,x);
+        int arr[] = {1,2,3,4,5,6};
+        int target = 9 ;
+        TwoSum(arr,target);
     }
-    public static void search(int[] arr,int x) {
+    public static void TwoSum(int arr[],int target) {
         for(int i=0;i<arr.length;i++){
-            if(x==arr[i]) {
-                System.out.println("founded");
-                 return ;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]+arr[j]==target){
+                    System.out.println(arr[i]+" "+arr[j]);
+                    System.out.println(i+" "+j);
+                    return ;
+               }
             }
-        } 
-        System.out.println("not founded");
+        }
+        System.out.println("Not founded");
     }
 }
+
