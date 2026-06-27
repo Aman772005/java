ff46d4f6a7c797235dcb4517eb5cba61142aae8b
Aman772005 <amansinghtomar772005@gmail.com>
2026-06-27 17:29:54 +0530

    Second max number in array

--- lecture_6/Questions.java
+++ lecture_6/Questions.java
@@ -45,24 +45,47 @@ package lecture_6;
 
 // *** question-3 *** 
 
+// public class Questions {
+
+//     public static void main(String[] args) {
+//         int arr[] = {1,2,3,4,5,6};
+//         int target = 9 ;
+//         TwoSum(arr,target);
+//     }
+//     public static void TwoSum(int arr[],int target) {
+//         for(int i=0;i<arr.length;i++){
+//             for(int j=i+1;j<arr.length;j++){
+//                 if(arr[i]+arr[j]==target){
+//                     System.out.println(arr[i]+" "+arr[j]);
+//                     System.out.println(i+" "+j);
+//                     return ;
+//                }
+//             }
+//         }
+//         System.out.println("Not founded");
+//     }
+// }
+
+
+// *** question-4 *** 
+
+import java.util.Arrays;
+
 public class Questions {
 
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6};
-        int target = 9 ;
-        TwoSum(arr,target);
-    }
-    public static void TwoSum(int arr[],int target) {
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]+arr[j]==target){
-                    System.out.println(arr[i]+" "+arr[j]);
-                    System.out.println(i+" "+j);
-                    return ;
-               }
+        int arr[] = {4, 10, 10, 6, 8};
+
+        Arrays.sort(arr);
+
+        int max = arr[arr.length - 1];
+
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] != max) {
+                System.out.println("Second max number is: " + arr[i]);
+                break;
             }
         }
-        System.out.println("Not founded");
     }
 }
 
