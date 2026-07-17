14551dd5a7c9eaf3089d59ddda9fc37865881157
Aman772005 <amansinghtomar772005@gmail.com>
2026-07-17 22:10:30 +0530

    sort , bubble sort

---
+++ lecture_7/bubblesort.java
@@ -0,0 +1,23 @@
+public class bubblesort {
+    public static void print(int arr[]) {
+        for(int elem : arr){
+            System.out.print(elem+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,-2,2,3,4,-5,0,2,7};
+        int n = arr.length-1;
+        print(arr);
+        for(int j=1;j<n-1;j++){
+            for(int i = 0;i<n;i++){
+                if(arr[i]>arr[i+1]){
+                    int temp = arr[i];
+                     arr[i] = arr[i+1] ;
+                     arr[i+1] = temp;
+                }
+            }
+        }
+        print(arr);
+    }
+}
