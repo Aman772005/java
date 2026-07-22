0ac23026f26af89c0ea833ba6ae7ab614ea99e86
Aman772005 <amansinghtomar772005@gmail.com>
2026-07-22 23:01:55 +0530

    descending order selection sorting

--- lecture_7/selectionSort.java
+++ lecture_7/selectionSort.java
@@ -1,3 +1,33 @@
+// from minimum 
+// public class selectionSort {
+
+//     public static void print(int arr[]) {
+//         for(int elem : arr){
+//             System.out.print(elem+" ");
+//         }
+//         System.out.println();
+//     }
+//     public static void main(String[] args) {
+//         int arr []= {5,-2,7,-1,0,8,9,};
+//         int n= arr.length;
+//         print(arr);
+//         for(int i=0;i<n-1;i++){
+//             int min = Integer.MAX_VALUE;
+//             int mindex = -1;
+//             for(int j = i;j<n;j++){
+//                 if(arr[j]<min){
+//                     min = arr[j];
+//                     mindex = j ;
+//                 }
+//             }
+//             int temp = arr[i];
+//             arr[i] = arr[mindex];
+//             arr[mindex] = temp;
+//         }
+//         print(arr);
+//     }
+// }
+
 public class selectionSort {
 
     public static void print(int arr[]) {
@@ -11,20 +41,21 @@ public class selectionSort {
         int n= arr.length;
         print(arr);
         for(int i=0;i<n-1;i++){
-            int min = Integer.MAX_VALUE;
-            int mindex = -1;
+            int max = Integer.MIN_VALUE;
+            int mandex = -1;
             for(int j = i;j<n;j++){
-                if(arr[j]<min){
-                    min = arr[j];
-                    mindex = j ;
+                if(arr[j]>max){
+                    max = arr[j];
+                    mandex = j ;
                 }
             }
             int temp = arr[i];
-            arr[i] = arr[mindex];
-            arr[mindex] = temp;
+            arr[i] = arr[mandex];
+            arr[mandex] = temp;
         }
         print(arr);
     }
 }
-// 9o3
-// 1034
+
+
+
