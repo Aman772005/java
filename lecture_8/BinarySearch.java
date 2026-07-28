b2c11dd9da5bb16e37973881698618fa8daa9f98
Aman772005 <amansinghtomar772005@gmail.com>
2026-07-28 14:21:36 +0530

    Binary search half

--- lecture_8/BinarySearch.java
+++ lecture_8/BinarySearch.java
@@ -1,15 +1,6 @@
 public class BinarySearch {
 
     public static void main(String[] args) {
-        int arr[] = {9,8,3,2,1,10,-4,-21};
-
-        int min = Integer.MAX_VALUE;
-
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]<min){
-                min=arr[i];
-            }
-        }
-        System.out.println(min);
+        
     }
 }
