5d92dd4e1729ad4d90958b36c4e7dd8fb2d28209
Aman772005 <amansinghtomar772005@gmail.com>
2026-07-28 14:18:48 +0530

    min

--- lecture_8/BinarySearch.java
+++ lecture_8/BinarySearch.java
@@ -1,5 +1,15 @@
 public class BinarySearch {
+
     public static void main(String[] args) {
-        System.out.println("hii");
+        int arr[] = {9,8,3,2,1,10,-4,-21};
+
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println(min);
     }
 }
