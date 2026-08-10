ad27414b9900cb9b1b104e07ae090cf35e646506
Aman772005 <amansinghtomar772005@gmail.com>
2026-08-10 10:34:37 +0530

    descending order binary search

--- lecture_8/BinarySearch.java
+++ lecture_8/BinarySearch.java
@@ -19,26 +19,46 @@
 // }
 
 
-public class BinarySearch {
+// public class BinarySearch {
 
-    public static int target(int arr[],int k) {
-        int lo = 0, hi = arr.length-1,idx = -1 ;
-        while(lo<=hi){
-            int mid = (lo+hi)/2;
-            if(arr[mid]>k) hi = mid - 1 ;
-            else if(arr[mid]<k) lo = mid + 1;
-            else{
-                idx = mid ;
-                hi = mid -1;
-            }
-        }
-        return idx ;
-    }
+//     public static int target(int arr[],int k) {
+//         int lo = 0, hi = arr.length-1,idx = -1 ;
+//         while(lo<=hi){
+//             int mid = (lo+hi)/2;
+//             if(arr[mid]>k) hi = mid - 1 ;
+//             else if(arr[mid]<k) lo = mid + 1;
+//             else{
+//                 idx = mid ;
+//                 hi = mid -1;
+//             }
+//         }
+//         return idx ;
+//     }
 
-    public static void main(String[] args) {
-        int arr [] = {1,1,2,2,2,3,3,4,4,5,5,5,6,6};
-        int k = 5;
-        System.out.print(target(arr,k));
-    }
-}
-// update 
+//     public static void main(String[] args) {
+//         int arr [] = {1,1,2,2,2,3,3,4,4,5,5,5,6,6};
+//         int k = 5;
+//         System.out.print(target(arr,k));
+//     }
+// }
+
+// public class BinarySearch{
+//     public static int search(int arr[],int target) {
+//         int hi = 0,lo = arr.length-1;
+//         while(hi<=lo){
+//             int mid = (lo+hi)/2;
+//             if(arr[mid] == target) return arr[mid];
+//             else if (arr[mid]>target) hi = mid +1 ;
+//             else lo = mid-1 ;
+//         }
+//         return -1 ;
+//     }
+//     public static void main(String[] args) {
+//         int arr[] = {100,90,80,70,60,50,40,30,20,10};
+//         int target = 30;
+//         System.out.println(search(arr,target));
+//     }
+// }
+
+
+
